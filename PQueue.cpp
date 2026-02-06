04e53c615fb6daeff921f674d9ef1aaac27ff445
MahirVallabhaneni <mahir.vallabhaneni@gmail.com>
2026-02-06 11:07:50 -0600

    Create PQueue.cpp

---
+++ PQueue.cpp
@@ -0,0 +1,94 @@
+/*
+ * LaQueue
+ * 
+ * 
+ * This is part of a series of labs for the Liberal Arts and Science Academy.
+ * The series of labs provides a mockup of an POSIX Operating System
+ * referred to as LA(SA)nix or LAnix.
+ *  
+ * (c) copyright 2018, James Shockey - all rights reserved
+ * 
+ * */
+
+#include <iostream>
+#include <cstdio>
+#include <cstring>
+#include <cstdlib>
+#include "PQueue.h"
+
+
+
+/*
+ * Class Priority Queue
+ */
+
+		
+/*
+* Insert into Priority Queue
+*/
+void PQueue::push(void *item, int priority)
+{
+	node* newNode = new node;
+	newNode->data = item;
+	newNode->priority = priority;
+	newNode->link = NULL;
+
+	if (front == NULL || priority < front->priority)
+	{
+		newNode->link = front;
+		front = newNode;
+		return;
+	}
+
+	node* temp = front;
+	while (temp->link != NULL && temp->link->priority <= priority)
+	{
+		temp = temp->link;
+	}
+
+	newNode->link = temp->link;
+	temp->link = newNode;
+}
+
+/*
+ * Delete from Priority Queue
+ */
+void* PQueue::top()
+{
+	if (front == NULL)
+		return NULL;
+	return front->data;
+}
+/*
+ * Delete from Priority Queue
+ */
+void PQueue::pop()
+{
+	if (front == NULL)
+		return;
+	node* temp = front;
+	front = front->link;
+	delete temp;
+}
+
+/*
+ * Print Priority Queue
+ */
+void PQueue::display()
+{
+node* ptr = front;
+	while (ptr != NULL)
+	{
+		std::cout << ptr->priority << " " << (char*)ptr->data << std::endl;
+		ptr = ptr->link;
+	}
+}
+long PQueue::getCur(){
+    return current;
+}
+long PQueue::getFlag(){
+    return flag;
+}
+void PQueue::setFlag(long flag){
+    this->flag=flag;
+}
